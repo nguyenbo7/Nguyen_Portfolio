dbf783c189015bc0bac8c72a12c299a760a6defd
Nguyen Chau <nguyenchaubo7@gmail.com>
2022-09-21 22:19:52 -0700

    Update the about section with NEW DESIGN: 	1. Fix the accordion with javascript 	2. Change img to left, about & accordion to right. 	3. Update the accordion__content.

--- app.js
+++ app.js
@@ -12,3 +12,21 @@ let swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     keyboard: true,
 });
+
+
+
+const accordionItems = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < accordionItems.length; i++) {
+    accordionItems[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+accordionItems.forEach(item => item.addEventListener('click', toggleAccordion));
